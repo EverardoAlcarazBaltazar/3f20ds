3422dcfdfcdec4a898b85d4c43c85dde84a3278d
EverardoAlcarazBaltazar <72171554+EverardoAlcarazBaltazar@users.noreply.github.com>
2020-11-18 16:34:20 -0600

    Add files via upload

---
+++ 1p/ordenamiento recursivo/primo.js
@@ -0,0 +1,25 @@
+var c = 100;
+var j = 2;
+var numerosPrimos = [];
+
+for (; j < c; j++) {
+
+  if (primo(j)) {
+    numerosPrimos.push(j);
+  }
+  
+}
+
+document.write(numerosPrimos);
+
+function primo(numero) {
+
+  for (var i = 2; i < numero; i++) {
+
+    if (numero % i === 0) {
+      return false;
+    }
+
+  }
+  return numero !== 1;
+}
